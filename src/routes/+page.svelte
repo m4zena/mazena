971ab46f98f679c76cd9c980fe38b93ea0abb882
Taher Alfayad <taheralfayad@gmail.com>
2026-03-21 12:52:14 -0400

    feat(landing): add dummy structure to test api

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { api } from "$lib/utils/api.svelte";
+
+  const makeRequest = () => {
+    api.post("/items", { description: "AGHAHGHAGHHGHHH" });
+  };
+</script>
+
+<h1>Hello!</h1>
+<button onclick={makeRequest}>make dat request</button>
