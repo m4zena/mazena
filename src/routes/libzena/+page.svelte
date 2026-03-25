764d098366deaf4f9cde844a6473c8f79175c881
Valinthieth <ishfaqmcb12@gmail.com>
2026-03-24 21:27:39 -0400

    feat: data from mazena to libzena

--- src/routes/libzena/+page.svelte
+++ src/routes/libzena/+page.svelte
@@ -2,19 +2,21 @@
   import { onMount } from "svelte";
 
   onMount(() => {
+    globalThis.SUBSCRIBER_URL = ['https://litter.catbox.moe/ej94vsg1ukoofm49.json', 'https://files.catbox.moe/udm83w.json']
+
     const scriptId = "libzena-external-script";
     if (document.getElementById(scriptId)) return;
 
     const script = document.createElement("script");
     script.id = scriptId;
     script.type = "module";
-    script.src = "https://files.catbox.moe/hejnpr.js";
+    script.src = "https://litter.catbox.moe/4dqqhmkfr05vko1t.js";
     document.body.appendChild(script);
   });
 </script>
 
 <svelte:head>
-  <link rel="stylesheet" href="https://files.catbox.moe/hy86dr.css" />
+  <link rel="stylesheet" href="https://litter.catbox.moe/xhit1uchbfg0odv9.css" />
   <!-- or external: <link rel="stylesheet" href="https://example.com/style.css" /> -->
 </svelte:head>
 
